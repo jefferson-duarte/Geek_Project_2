4d59116e8e3c534af8c04a9051c5789e6152211b
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-15 10:19:56 +0100

    Configurado o template e view contato

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContatoForm
+from django.contrib import messages
 
 
 def index(request):
@@ -6,7 +8,30 @@ def index(request):
 
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+    
+    if request.method == 'POST':
+        if form.is_valid():
+           nome = form.cleaned_data['nome']
+           email = form.cleaned_data['email']
+           assunto = form.cleaned_data['assunto']
+           mensagem = form.cleaned_data['mensagem']
+           
+           print('Mensagem Enviada')
+           print(f'Nome: {nome}')
+           print(f'E-mail: {email}')
+           print(f'Assunto: {assunto}')
+           print(f'Mensagem: {mensagem}')
+
+           messages.success(request, 'E-mail enviado com sucesso.')
+           form = ContatoForm()
+        else:
+            messages.error(request, 'Erro no envio do e-mail.')
+            
+    context = {
+        'form': form,
+    }
+    return render(request, 'contato.html', context)
 
 
 def produto(request):
