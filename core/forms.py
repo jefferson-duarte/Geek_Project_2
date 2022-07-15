017a03a4c4a5639a164073878e13e056aebcc31e
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-15 10:40:51 +0100

    Configurado envio de emails

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.mail.message import EmailMessage
 
 
 class ContatoForm(forms.Form):
@@ -6,3 +7,20 @@ class ContatoForm(forms.Form):
     email = forms.EmailField(label='E-mail', max_length=100)
     assunto = forms.CharField(label='Assunto', max_length=120)
     mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+        
+        conteudo = f'Nome: {nome}\nE-mail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}'
+        
+        mail = EmailMessage(
+            subject='Email enviado pelo sistema django2.',
+            body=conteudo,
+            from_email='contato@email.com',
+            to=['contato@globo.com'],
+            headers={'Replay-To': email}
+        )
+        mail.send()
--- core/views.py
+++ core/views.py
@@ -12,17 +12,8 @@ def contato(request):
     
     if request.method == 'POST':
         if form.is_valid():
-           nome = form.cleaned_data['nome']
-           email = form.cleaned_data['email']
-           assunto = form.cleaned_data['assunto']
-           mensagem = form.cleaned_data['mensagem']
+           form.send_mail()
            
-           print('Mensagem Enviada')
-           print(f'Nome: {nome}')
-           print(f'E-mail: {email}')
-           print(f'Assunto: {assunto}')
-           print(f'Mensagem: {mensagem}')
-
            messages.success(request, 'E-mail enviado com sucesso.')
            form = ContatoForm()
         else:
